fbefd105f2cd8d78eb31d3b8ac4fec842a573963
maria <mariat1818@da7m.net>
2024-08-25 02:25:02 +0900

    feat: 리퀘스트 기본 클래스 추가

---
+++ airflow/tasks/utils/default_request.py
@@ -0,0 +1,22 @@
+import requests
+
+class Request(object):
+
+    def __init__(self, **kwargs):
+        """
+
+        Args:
+            **kwargs:
+                site: sec(https://www.sec.gov/) 이곳은 헤더에 이름 및 이메일을 적어야함
+        """
+        self.headers = {
+            "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/127.0.0.0 Safari/537.36",
+            "Accept-Encoding": "gzip, deflate, br, zstd",
+            "Accept": "*/*"
+        }
+        if kwargs.get('site', None) == "SEC":
+            self.headers.update({"User-Agent": "test mariat1717@daum.net"})
+
+    def get(self, url, headers: dict = {}):
+        self.headers.update(headers)
+        return requests.get(url, headers=self.headers)
