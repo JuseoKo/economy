9768b9c0c91283fdc76c894dbb0d97c57ae8d485
JuseoKo <mariat1717@daum.net>
2025-03-02 03:22:15 +0900

    feat : shortbalance table

---
+++ pipeline/table/models/stock/fact_short_balance.py
@@ -0,0 +1,19 @@
+from sqlalchemy import Column, String, Float, Date, ForeignKey, BigInteger, PrimaryKeyConstraint
+from sqlalchemy.orm import relationship
+from ...base import Base
+
+# 팩트 테이블 (공매도 잔고)
+class FactStockShortBalance(Base):
+    __tablename__ = 'fact_stock_short_balance'
+
+    ucode = Column(String, ForeignKey('dim_company.ucode'), primary_key=True)
+    date = Column(Date, nullable=False) # 년월일
+    short_volume = Column(BigInteger, nullable=False)  # 공매도 잔고(주식 수)
+    short_ratio = Column(Float, nullable=False)  # 공매도 비율 (%)
+
+    company = relationship("CompanyDimension", back_populates="fact_stock_short_balance")
+
+
+    __table_args__ = (
+        PrimaryKeyConstraint('ucode', 'date', name='pk_fact_stock_short_balance_date_ucode'),
+    )
