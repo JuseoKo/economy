1602bbd7e4501ac6341912284f50036718e1ba61
JuseoKo <mariat1717@daum.net>
2025-07-16 00:20:21 +0900

    ref: Remove test prefix from run function

--- pipeline/runs/stock/dart_runs.py
+++ pipeline/runs/stock/dart_runs.py
@@ -1,9 +1,9 @@
 from pipeline.tasks.stock import dart
-import pytest
 
-def test_dart_performance_list_run():
+
+def dart_performance_list_run():
     dart.DartPerformanceList().run("DART 재무 데이터 수집 목록 수집")
 
 
-def test_dart_performance_run():
+def dart_performance_run():
     dart.DartPerFormance().run("DART 재무 데이터 수집")
--- pipeline/runs/stock/krx_runs.py
+++ pipeline/runs/stock/krx_runs.py
@@ -1,11 +1,10 @@
 from pipeline.tasks.stock import krx
-import pytest
 
 
-def test_stock_list_run():
-    res = krx.StockList().run("KRX 주식 리스트 수집")
+def stock_list_run():
+    krx.StockList().run("KRX 주식 리스트 수집")
 
 
-def test_stock_price_run():
-    res = krx.StockPrice().run("KRX 주가 목록 수집")
+def stock_price_run():
+    krx.StockPrice().run("KRX 주가 목록 수집")
 
